a44c0d4e03f9338535220773b164254772a94fc4
Thamidu-Nadun <nadunrz101@gmail.com>
2025-07-21 23:39:31 +0530

    Modified CommentArticle.java

---
+++ src/main/java/nadun_blog/model/CommentArticle.java
@@ -0,0 +1,37 @@
+package nadun_blog.model;
+
+import java.sql.Date;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import nadun_blog.model.post.Article;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentArticle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Article post;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private Date createdAt;
+    private String comment;
+}
