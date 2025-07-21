fae5b8e2a5a37d1fb1b1b397a347ce62b2d1fbe7
Thamidu-Nadun <nadunrz101@gmail.com>
2025-07-21 23:25:10 +0530

    Modified DataAccessFailure Exception to support message parameter

--- src/main/java/nadun_blog/util/exceptions/DataAccessFailureException.java
+++ src/main/java/nadun_blog/util/exceptions/DataAccessFailureException.java
@@ -4,4 +4,8 @@ public class DataAccessFailureException extends Exception {
     public DataAccessFailureException() {
         super("Failed to access data");
     }
+
+    public DataAccessFailureException(String message) {
+        super(message);
+    }
 }
